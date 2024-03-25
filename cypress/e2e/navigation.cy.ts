bac0d7257392e2d07a560ee7ba2e92c639e8bd9d
noboa <62807778+nobadev@users.noreply.github.com>
2024-03-25 02:31:14 +0000

    Added support button functionality (#5)

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -36,7 +36,7 @@ describe("Sidebar Navigation", () => {
       cy.get("nav").contains("Collapse").click();
 
       // check that links still exist and are functionable
-      cy.get("nav").find("a").should("have.length", 5).eq(1).click();
+      cy.get("nav").find("a").should("have.length", 6).eq(1).click();
       cy.url().should("eq", "http://localhost:3000/dashboard/issues");
 
       // check that text is not rendered
@@ -80,7 +80,7 @@ describe("Sidebar Navigation", () => {
       isInViewport("nav");
 
       // check that all links are rendered
-      cy.get("nav").find("a").should("have.length", 5);
+      cy.get("nav").find("a").should("have.length", 6);
 
       // Support button should be rendered but Collapse button not
       cy.get("nav").contains("Support").should("exist");
@@ -90,6 +90,20 @@ describe("Sidebar Navigation", () => {
       cy.get("img[alt='close menu']").click();
       cy.wait(500);
       isNotInViewport("nav");
+    });
+  });
+
+  context("support email", () => {
+    beforeEach(() => {
+      cy.viewport(1025, 900);
+    });
+
+    it("contains mailto in the href for the support button", () => {
+      const mailToRequest =
+        "mailto:support@prolog-app.com?subject=Support Request:";
+      cy.get("nav")
+        .contains("Support")
+        .should("have.attr", "href", mailToRequest);
     });
   });
 });
